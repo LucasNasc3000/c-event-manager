eb9745cdce2d4d6218ed325d65d107b4b0ec62d3
Lucas <storagemail329@gmail.com>
2024-06-08 21:21:17 -0300

    classe UserAdmin criada

--- src/EmployeeAbstract.ts
+++ src/EmployeeAbstract.ts
@@ -1,11 +1,15 @@
 import { prisma } from '../lib/prisma';
 
 export abstract class Employee {
-  constructor(
-    private _name: string,
-    private _email: string,
-    private _password: string,
-  ) {}
+  protected _name: string;
+  protected _email: string;
+  protected _password: string;
+
+  constructor(name: string, email: string, password: string) {
+    this._name = name;
+    this._email = email;
+    this._password = password;
+  }
 
   get name(): string {
     return this._name;
@@ -23,7 +27,7 @@ export abstract class Employee {
     this._email = email;
   }
 
-  async Create() {
+  protected async Create() {
     const createEmployee = await prisma.employee.create({
       data: {
         name: this._name,
@@ -32,18 +36,17 @@ export abstract class Employee {
       },
     });
 
-    console.log(
-      `Funcionario criado: ${(createEmployee.name, createEmployee.email)}`,
-    );
+    console.log('Funcionario criado: \n');
+    console.table(`${(createEmployee.name, createEmployee.email)}`);
   }
 
-  async EmployeeList() {
+  protected async EmployeeList() {
     const employeesList = await prisma.employee.findMany();
 
     return console.table(employeesList);
   }
 
-  async Update(id: string) {
+  protected async Update(id: string) {
     const findEmployee = await prisma.employee.findUnique({
       where: {
         id: id,
@@ -70,7 +73,7 @@ export abstract class Employee {
     );
   }
 
-  async Delete(id: string) {
+  protected async Delete(id: string) {
     const deleteEmployee = await prisma.employee.delete({
       where: {
         id: id,
@@ -81,4 +84,58 @@ export abstract class Employee {
   }
 }
 
-export class UserAdmin extends Employee {}
+export class UserAdmin {
+  private static adminExists: boolean;
+  private adminEmail: string;
+  private adminPassword: string;
+
+  constructor(adminEmail: string, adminPassword: string) {
+    this.adminEmail = adminEmail;
+    this.adminPassword = adminPassword;
+    this.findAdmin();
+  }
+
+  private async findAdmin() {
+    const admExists = await prisma.employee.findUnique({
+      where: {
+        email: this.adminEmail,
+        password: this.adminPassword,
+      },
+    });
+
+    if (admExists === null) {
+      UserAdmin.adminExists = false;
+      return null;
+    }
+
+    UserAdmin.adminExists = true;
+    return admExists;
+  }
+
+  private async Create() {
+    const admExists = this.findAdmin();
+    if (admExists === null) {
+      const createAdmin = await prisma.adminLogin.create({
+        data: {
+          adminUser: this.adminEmail,
+          adminPassword: this.adminPassword,
+        },
+      });
+      console.log('Administrador criado: \n');
+      console.table(`${createAdmin.adminUser}`);
+    }
+    return 'Admin já existente';
+  }
+
+  static async GetAdmin(adminEmail: string, adminPassword: string) {
+    const admin = new UserAdmin(adminEmail, adminPassword);
+    const admExists = await admin.findAdmin();
+
+    if (admExists !== null) {
+      return admin.adminEmail;
+    }
+
+    await admin.Create();
+    return console.log('Administrador criado');
+  }
+}
